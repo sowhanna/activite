571791e75f132d32ac832a4f4f6a14bd90421da8
sowhanna <annasow1@esp.sn>
2024-12-21 10:53:21 +0100

    ajouter des fichiers

--- api.py
+++ api.py
@@ -9,5 +9,7 @@ def extract():
     features = extract_features(file)
     return jsonify(features)
 
+#if __name__ == "__main__":
+   # app.run(debug=True)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
